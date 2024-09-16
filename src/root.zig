475b0d94c871b169f13d2833a5432039cf6db449
aphirr1 <aphirr@proton.me>
2024-09-15 23:09:48 -0500

    Updated root.zig 'Color' struct wording.

--- src/root.zig
+++ src/root.zig
@@ -2,7 +2,7 @@ pub const timers = @import("timers.zig");
 
 pub const keypad: *volatile Keypad = @ptrFromInt(0x400_0130);
 
-/// Every color can be from 0-31.
+/// Works like rgb but very color can only be a value from 0 to 31.
 pub const Color = packed struct(u16) {
     transparent: bool = false,
     red: u5,
