21884c9d21b5bb9987a8d31773f1cabeb7d933d4
aphirr <aphirr@nix.os>
2024-09-27 20:18:20 -0500

    added back entry point

--- src/gba.zig
+++ src/gba.zig
@@ -29,3 +29,25 @@ const KeyState = enum(u1) {
     up = 1,
     down = 0,
 };
+
+export fn gMain() void {
+    if (@hasDecl(root, "main")) {
+        root.main();
+    } else {
+        @compileError("root file has no member named 'main'");
+    }
+}
+
+export fn _start() linksection(".text._start") callconv(.Naked) noreturn {
+    asm volatile (
+        \\.arm
+        \\.cpu arm7tdmi
+        //
+        \\b end_of_header 
+        \\.space 0xE0
+        \\end_of_header:
+        \\ldr r12, =gMain
+        \\bx r12    
+    );
+    while (true) {}
+}
